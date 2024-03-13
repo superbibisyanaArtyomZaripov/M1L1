3ca58aacea800fe8d331386184c34d3130d0005b
superbibisyanaArtyomZaripov <160399716+superbibisyanaArtyomZaripov@users.noreply.github.com>
2024-03-13 18:51:20 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,41 @@
-meme_dict = {
-            "КРИНЖ": "Что-то очень странное или стыдное",
-            "ЛОЛ": "Что-то очень смешное",
-            "РОФЛ": "Шутка",
-            "ЩИЩ": "одобрение или восторг",
-            "КРИПОВЫЙ": "страшный пугающий",
-            "АГРИТЬСЯ": "злиться"
-            }
-word = input("Введите непонятное слово (большими буквами!): ")
-if word in meme_dict.keys():
-    # Что делать, если слово нашлось?
-    print(meme_dict[word])
-else:
-    # Что делать, если слово не нашлось?
-    print("мы незнаем такого слова")
+import discord
+import random
+from discord.ext import commands
+
+
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print('We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send('Привет! Я бот {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+@bot.command()
+async def choose(ctx, *choices: str):
+    """Chooses between multiple choices."""
+    await ctx.send(random.choice(choices))
+@bot.command()
+async def joined(ctx, member: discord.Member):
+    """Says when a member joined."""
+    await ctx.send(f'{member.name} joined {discord.utils.format_dt(member.joined_at)}')
+
+
+
+
+bot.run("MTIxNDIwODcyMzEwNzQ1NDk4Ng.Gq7wAt.7JO1AI24s5pLdOtQMpec9blYDQtqLJkuOJHF5U")
+
